993ebebdd266f4ed1133d0fbc539493a15836bcc
Saravanakumar R <Saravanaramaswamy2003@gmail.com>
2025-02-13 22:02:42 +0530

    refactor: Simplify upload and download functions; update Vercel configuration for cleaner routing

--- app.py
+++ app.py
@@ -13,48 +13,43 @@ fieldnames_order = [
 ]
 
 @app.route('/')
-def upload_form(): # This function is called when the user visits the homepage
-    return render_template('upload.html') # This function returns the upload.html template
+def upload_form():
+    return render_template('upload.html')
 
 @app.route('/', methods=['POST'])
-def upload_files(): 
-    uploaded_files = request.files.getlist("files[]") # Get the uploaded files from the request
-    output_rows = [] # This list will contain the converted data from the uploaded files
+def upload_files():
+    uploaded_files = request.files.getlist("files[]")
+    output_rows = []
 
-    for file in uploaded_files: # Iterate through the uploaded files
-        if file.filename == '': # If the user did not select a file, skip it
-            continue 
+    for file in uploaded_files:
+        if file.filename == '':
+            continue
 
-        filename = file.filename # Get the filename of the uploaded file
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename) # Create the path to save the file to
-        file.save(file_path) # Save the file to the path
+        filename = file.filename
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
 
-        converted_data = convert_dat_to_csv(file_path) # Convert the file to a list of dictionaries
-        output_rows.extend(converted_data) # Add the converted data to the output_rows list
+        converted_data = convert_dat_to_csv(file_path)
+        output_rows.extend(converted_data)
 
-    # Filter out non-dictionary objects from output_rows
     output_rows = [row for row in output_rows if isinstance(row, dict)]
 
     if output_rows:
         output_file_path = os.path.join(app.config['CONVERTED_FOLDER'], 'output.csv')
 
-        # Derive fieldnames from the keys of dictionaries in output_rows
         fieldnames = set()
         for row in output_rows:
             fieldnames.update(row.keys())
 
-        # Order the fieldnames according to the specified order
         ordered_fieldnames = [fieldname for fieldname in fieldnames_order if fieldname in fieldnames]
         remaining_fieldnames = [fieldname for fieldname in fieldnames if fieldname not in ordered_fieldnames]
         ordered_fieldnames.extend(remaining_fieldnames)
 
-        # Write the output_rows to a CSV file
         with open(output_file_path, "w", newline="") as f:
             writer = csv.DictWriter(f, fieldnames=ordered_fieldnames, restval="N/A")
             writer.writeheader()
             writer.writerows(output_rows)
 
-        # Return the output file to the user
         return redirect(url_for('download_file', filename='output.csv'))
     else:
         return "Conversion failed. Please try again."
@@ -65,6 +60,7 @@ def download_file(filename):
         return send_file(os.path.join(app.config['CONVERTED_FOLDER'], filename), as_attachment=True)
     except Exception as e:
         return str(e)
-# Run the app
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
